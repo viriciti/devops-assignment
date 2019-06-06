3f371d54d72d7cfaf57ed8f07aea886a33fc570a
iklop <i.klop@viriciti.com>
2019-06-06 14:00:29 +0200

    Add tests to web-server

---
+++ web-server/test/app_test.js
@@ -0,0 +1,28 @@
+const app = require('../app')
+const request = require('request')
+const { expect } = require('request')
+
+describe('the test', function() {
+	it('should start', function(done) {
+		app.start(done)
+	})
+
+	it('should should respond', function(done) {
+		const req = {
+			json: true,
+			uri: 'http://localhost:3000/',
+		}
+
+		request.get(req, (error, res, body) => {
+			if (error) return done(error)
+			if (res.statusCode !== 200) return done(new Error(`oh no! ${res.statusCode}`))
+			console.log('body', body)
+			done()
+		})
+		
+	})
+
+	it('should stop', function(done) {
+		app.stop(done)
+	})
+})
