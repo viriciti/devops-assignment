bd10f84711e4305a23b437e4d80316ddeb15c125
iklop <i.klop@viriciti.com>
2020-05-26 18:12:39 +0200

    Add metrics and upgrade node

--- web-server/app.js
+++ web-server/app.js
@@ -1,12 +1,33 @@
-const express = require("express")
-const http    = require("http")
-const app     = express()
+const express    = require("express")
+const http       = require("http")
+const app        = express()
+const promClient = require('prom-client');
 
 let server
 
+requestSuccess = new promClient.Counter({
+	name: "request_success",
+	help: "Number of successful requests"
+})
+
+requestFailed = new promClient.Counter({
+	name: "request_failed",
+	help: "Number of failed requests"
+})
+
 app.get("/", (req, res) => {
 	res.json({ message: `${new Date} w00t! It's working`})
+	requestSuccess.inc(1)
 })
+
+app.get("/fail", (req, res) => {
+	res.json({ message: `${new Date} Oh! It's not working`})
+	requestFailed.inc(1)
+})
+
+app.get("/metrics", (req, res) => {
+	res.end(promClient.register.metrics());
+});
 
 module.exports = {
 	start(cb) {
